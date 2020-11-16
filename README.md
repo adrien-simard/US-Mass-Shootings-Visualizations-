9783000cc55b0525c9721c83d0b9889e3b19d490
Nowtryz <nowtryz.dev@gmail.com>
2020-11-16 15:34:10 +0100

    Get crimes per state

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 - [Info](https://www.motherjones.com/politics/2012/12/mass-shootings-mother-jones-full-data/)
 - [Dataset](https://docs.google.com/spreadsheets/d/1b9o6uDO18sLxBqPwl_Gh9bnhW-ev_dABH83M5Vb5L8o)
 - [Example](https://rstudio-pubs-static.s3.amazonaws.com/383516_df60fde7bead48f29cd2ce7799a3eab9.html)
+- [Map D3.js](http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922#stateslived.csv)
 
 ![Target result](target.png)
--- index.js
+++ index.js
@@ -1,4 +1,53 @@
+const extractData = data => {
+    const states = {}
+
+    data.forEach(crime => {
+        const state = crime.city.match(/, (.*)$/)[1]
+        if (state in states) {
+            states[state].crimes.push(crime)
+            states[state].fatalities += +crime.fatalities
+            states[state].injured += +crime.injured
+        }
+        else states[state] = {
+            state,
+            crimes: [crime],
+            fatalities: +crime.fatalities,
+            injured: +crime.injured
+        }
+    })
+
+    return [states, data.columns]
+}
+
+const display = states => {
+    //Width and height of map
+    const width = 960
+    const height = 500
+
+    // D3 Projection
+    const projection = d3.geo.albersUsa()
+        .translate([width/2, height/2])    // translate to center of screen
+        .scale([1000]);          // scale things down so see entire US
+
+    // Define path generator
+    const path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+        .projection(projection)  // tell path generator to use albersUsa projection
+
+    //Create SVG element and append map to the SVG
+    const svg = d3.select("#map")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+
+    // Append Div for tooltip to SVG
+    const div = d3.select("#map")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+
+
+}
+
 d3.csv('Mass-Shootings-1982-2020.csv')
-.then(data => {
-    console.log(data)
-})
+    .then(extractData)
+    .then(([states]) => display(states))
